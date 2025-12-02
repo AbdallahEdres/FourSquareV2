e63f7ee387f3b272d1a747cde2a0ce402be568e2
AbdallahEdres <abdallahedres005@gmail.com>
2025-12-02 18:37:13 +0200

    Initial commit

---
+++ FourSquareV2/FourSquareV2App.swift
@@ -0,0 +1,17 @@
+//
+//  FourSquareV2App.swift
+//  FourSquareV2
+//
+//  Created by Abdallah Edres on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FourSquareV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
