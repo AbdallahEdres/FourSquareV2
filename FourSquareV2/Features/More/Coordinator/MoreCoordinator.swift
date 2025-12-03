599205e5e0b5fc81f42fd67649097e3d5b6bda68
AbdallahEdres <abdallahedres005@gmail.com>
2025-12-03 22:47:57 +0200

    Introduced more coordinator
    Created more coordinator protocol as an abstract for more menu feature routing functionalities
    Created More coordinator that confirms to the protocol and handles navigations
    Created more routes to represent all more feature possible destinations

---
+++ FourSquareV2/Features/More/Coordinator/MoreCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  MoreCoordinator.swift
+//  FourSquareV2
+//
+//  Created by Abdallah Edres on 03/12/2025.
+//
+
+import SwiftUI
+import Combine
+
+final class MoreCoordinator: MoreCoordinatorProtocol{
+    @Published var path: NavigationPath = .init()
+    private var root: AnyView?
+
+    func start() -> AnyView {
+        if root == nil { root = AnyView(EmptyView())}
+        return root!
+    }
+
+    func viewFor(_ route: MoreRoutes) -> AnyView {
+        switch route {
+        default:
+            return AnyView(EmptyView())
+        }
+    }
+
+}
+
+extension MoreCoordinator{
+
+}
