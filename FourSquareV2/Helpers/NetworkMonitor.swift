4e116f16c9b550c90cd7c9224cb4e0719c10a627
AbdallahEdres <abdallahedres005@gmail.com>
2025-12-03 22:11:39 +0200

    Create NetworkMonitor.swift

---
+++ FourSquareV2/Helpers/NetworkMonitor.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkMonitor.swift
+//  FourSquareV2
+//
+//  Created by Abdallah Edres on 03/12/2025.
+//
+
+import Network
+import Combine
+
+final class NetworkMonitor: ObservableObject {
+    static let shared = NetworkMonitor()
+    
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitorQueue")
+    
+    @Published private(set) var isConnected: Bool = true
+    
+    private init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
